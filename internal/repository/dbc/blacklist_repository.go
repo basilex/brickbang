a6d7c58c0cb92cf0fbadb5d51d3baa45ccb2a3eb
Alexander Vasilenko <alexander.vasilenko@gmail.com>
2025-11-13 09:23:42 +0200

    CodeRabbit fixes

--- internal/repository/dbc/blacklist_repository.go
+++ internal/repository/dbc/blacklist_repository.go
@@ -2,33 +2,43 @@ package dbc
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
+const redisBlacklistKey = "blacklist:"
+
 type IBlacklistRepository interface {
-	Add(jti string, ttl time.Duration) error
-	Exists(jti string) (bool, error)
+	Add(ctx context.Context, jti string, ttl time.Duration) error
+	Exists(ctx context.Context, jti string) (bool, error)
 }
 
 type blacklistRepository struct {
-	ctx    context.Context
 	client *redis.Client
 }
 
-func NewBlacklistRepository(ctx context.Context, client *redis.Client) IBlacklistRepository {
+func NewBlacklistRepository(client *redis.Client) IBlacklistRepository {
 	return &blacklistRepository{
-		ctx:    ctx,
 		client: client,
 	}
 }
 
-func (rcv *blacklistRepository) Add(jti string, ttl time.Duration) error {
-	return rcv.client.Set(rcv.ctx, "blacklist:"+jti, true, ttl).Err()
+func (rcv *blacklistRepository) Add(ctx context.Context, jti string, ttl time.Duration) error {
+	if jti == "" {
+		return fmt.Errorf("jti cannot be empty")
+	}
+
+	return rcv.client.Set(ctx, "blacklist:"+jti, true, ttl).Err()
 }
 
-func (rcv *blacklistRepository) Exists(jti string) (bool, error) {
-	res, err := rcv.client.Exists(rcv.ctx, "blacklist:"+jti).Result()
+func (rcv *blacklistRepository) Exists(ctx context.Context, jti string) (bool, error) {
+	if jti == "" {
+		return false, fmt.Errorf("jti cannot be empty")
+	}
+
+	res, err := rcv.client.Exists(ctx, redisBlacklistKey+jti).Result()
+
 	return res > 0, err
 }

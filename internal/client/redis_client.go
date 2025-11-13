6984550bc2e9eca36dc7910070f5e046b17d3f95
Alexander Vasilenko <alexander.vasilenko@gmail.com>
2025-11-13 13:11:17 +0200

    CodeRabbit fixes

--- internal/client/redis_client.go
+++ internal/client/redis_client.go
@@ -1,16 +1,13 @@
 package client
 
 import (
-    "context"
-    "github.com/redis/go-redis/v9"
+	"github.com/redis/go-redis/v9"
 )
 
-var Ctx = context.Background()
-
 func NewRedisClient(addr, password string, db int) *redis.Client {
-    return redis.NewClient(&redis.Options{
-	Addr:     addr,
-	Password: password,
-	DB:       db,
-    })
+	return redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
 }
